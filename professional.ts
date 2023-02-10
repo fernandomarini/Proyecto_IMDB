21e59151a97b64934d603f33ff8ce6c046003855
Fernando Marini <fer.ms114@gmail.com>
2023-02-10 11:35:16 -0300

    Professional.01

---
+++ professional.ts
@@ -0,0 +1,37 @@
+
+class Professional {
+    // Declaracion de atributos en Constructor
+    
+    // Constructor 
+    constructor(public name: string,
+        public age: number,
+        public weight: number,
+        public height: number,
+        public isRetired: boolean,
+        public nationality: string,
+        public oscarsNumber: number,
+        public profession: string ){};
+
+
+    // Metodos
+    public printConsolePar( ):string {
+        let strinProf: string = "";
+        strinProf = (
+            `Name ${this.name} \n
+            Age: ${this.age} \n
+            Weight: ${this.weight} \n
+            Height: ${this.height} \n
+            isRetired: ${this.isRetired} \n
+            Nationality: ${this.nationality} \n
+            Ocar: ${this.oscarsNumber} \n
+            Profession : ${this.profession} \n`)
+        return strinProf;
+        console.log(strinProf);
+    };
+
+};
+
+
+
+
+export { Professional }
---
+++ testProfessional.ts
@@ -0,0 +1,8 @@
+import { Professional } from "./professional";
+
+
+let film1 = new Professional("Nombre1",43,52,21,true,"Frances",1,"Programador" );
+
+console.log(film1);
+
+film1.printConsolePar();
