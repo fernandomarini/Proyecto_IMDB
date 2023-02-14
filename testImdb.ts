ca255d9de1eaa9b5307b6cec7084d708cca9f8d3
Fernando Marini <fer.ms114@gmail.com>
2023-02-14 08:20:37 -0300

    peligro.01

--- testImdb.ts
+++ testImdb.ts
@@ -1,6 +1,9 @@
 import { Movie } from "./movie";
 import { Imdb } from "./imdb";
 
+const fs = require("fs");
+
+
 let peli1 = new Movie("Programacion1",2023,"Espanyola","ficcion");
 let peli2 = new Movie("Programacion2",2022,"Argentina","documental");
 
@@ -9,3 +12,9 @@ let arrPelis: Movie[] = [peli1,peli2];
 let imdbTest = new Imdb( arrPelis );
 
 console.log( imdbTest );
+
+console.log(JSON.stringify(imdbTest));
+
+fs.writeFileSync("ImdbBBDD.json", JSON.stringify(imdbTest));
+
+   
